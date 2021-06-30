3b9060473059bf3b71573cf5bbcf35ad765c9769
willams <desenvolvimento@appis.com.br>
2021-06-30 07:19:11 -0300

    upload last version file

--- extra_script.py
+++ extra_script.py
@@ -2,6 +2,9 @@ Import("env")
 
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
-print(defines)
+
+version_file = open(".pio/build/esp32dev/last_version.txt", "w+")
+version_file.write(defines.get("VERSION"))
+version_file.close()
 
 env.Replace(PROGNAME="firmware_%s" % defines.get("VERSION"))
